87c59b10fc766e1bab17b9edde51eef2152d341b
David Flanik <daveflanik@gmail.com>
2023-03-18 21:04:00 +0900

    added simple middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express(); // creating the API
 
+//Setting up middleware
+app.use(cors()); // cross origin research sharing
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+//setting up routes
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
