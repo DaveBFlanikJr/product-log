85b8cc6d5ed590ee08e7249e3c6088e81354848f
David Flanik <daveflanik@gmail.com>
2023-03-19 20:30:03 +0900

    set up signIn function

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createNewUser = async (req, res) => {
   const user = await prisma.user.create({
@@ -9,5 +9,25 @@ export const createNewUser = async (req, res) => {
     },
   });
   const token = createJWT(user);
+  res.json({ token });
+};
+
+export const signIn = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    //Step 1 check if there is even a user
+    where: {
+      // checking to see if the user name that is passed in the client is the same as the one in the db
+      username: req.body.username,
+    },
+  });
+  // step 2 look at that users hashed PW and see if it match the plain text PW
+  const isValid = await comparePasswords(req.body.password, user.password); // using the helper function we check is plain text PW is the same as the hashed PW
+
+  if (!isValid) {
+    res.status(401);
+    res.json({ message: "Incorrect Password" });
+    return;
+  }
+  const token = createJWT(user); // if it is valid create the token so they get access to the API
   res.json({ token });
 };
